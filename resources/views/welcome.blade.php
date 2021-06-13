4f95730699b97da339b0623645d8dd5d07ab478b
naostudy0 <naostudy0@gmail.com>
2021-06-14 00:10:05 +0900

    [update]アプリの紹介ページを作成

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,97 +4,60 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>予定管理n.u</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
 
         <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
+        <link href="{{ asset('css/reset.css') }}" rel="stylesheet">
+        <link href="{{ asset('css/welcome.css') }}" rel="stylesheet">
     </head>
     <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
+        <div class="main">
+            <header>
+                <h1>予定管理n.u</h1>
+                <nav class="links">
+                    @if (Route::has('login'))
+                        @auth
+                            <a href="{{ url('/schedule') }}">予定管理へ</a>
+                        @else
+                            <a href="{{ route('login') }}">ログイン</a>
 
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
+                            @if (Route::has('register'))
+                                <a href="{{ route('register') }}">会員登録</a>
+                            @endif
+                        @endauth
+                    @endif
+                </nav><!-- links -->
+            </header>
 
             <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
+                <section class="sec1">
+                    <img src="{{ asset('image/schedule_1280.jpg') }}" alt="予定表">
+                </section><!-- sec1 -->
 
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
-            </div>
-        </div>
+                <section class="sec sec2">
+                    <h2>予定を色分けして管理</h2>
+                    <div class="explanation">
+                        <p>カテゴリごとに予定を色分けして登録できます。</p>
+                        <img src="{{ asset('image/post-it_640.png') }}" class="post-it" alt="付箋">
+                    </div>
+                    <img src="{{ asset('image/device_1280.png') }}" class="screen" alt="アプリのイメージ">
+                </section><!-- sec2 -->
+
+                <section class="sec sec3">
+                    <h2>本アプリは就職活動用のポートフォリオです</h2>
+                    <p>どなたでも無料でご利用いただけますが、以下の点にご注意ください。</p>
+                    <ul>
+                        <li>予告なく機能の追加・削除、レイアウトの変更を行う場合があります。</li>
+                        <li>登録した予定や会員情報の修正・削除は、基本的に行いません。万が一行う場合は、アプリ内で事前告知をした上で行います。</li>
+                    </ul>
+                </section><!-- sec3 -->
+            </div><!-- content -->
+        </div><!-- main -->
+        <footer>
+            <small>&copy 2021 予定管理n.u</small>
+        </footer>
     </body>
 </html>
