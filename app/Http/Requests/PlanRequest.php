7b6cd8025244cf36a75e70d0cffd9c4931e9d81d
nao.u <nao.study0@gmail.com>
2022-09-19 00:08:44 +0900

    [clear]リファクタリング

--- app/Http/Requests/PlanRequest.php
+++ app/Http/Requests/PlanRequest.php
@@ -3,11 +3,9 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
-use DateTime;
 
 class PlanRequest extends FormRequest
 {
-
     /**
      * Determine if the user is authorized to make this request.
      *
@@ -15,7 +13,6 @@ class PlanRequest extends FormRequest
      */
     public function authorize()
     {
-
         return true;
     }
 
@@ -26,50 +23,51 @@ class PlanRequest extends FormRequest
      */
     public function rules()
     {
-        $formData = $this->all();
+        $form_data = $this->all();
 
-        $start_date = strtotime($formData["start_date"]);
-        $end_date = strtotime($formData["end_date"]);
-        $start_time = new DateTime($formData["start_time"]);
-        $end_time = new DateTime($formData["end_time"]);
+        $start_date = strtotime($form_data["start_date"]);
+        $end_date = strtotime($form_data["end_date"]);
 
-
-        
         // 終了日が開始日よりも後であれば開始時間と終了時間が前後しても良い
-        if($start_date < $end_date){
+        if ($start_date < $end_date) {
             return [
                 'start_date' => 'required|date',
                 'start_time' => 'required|date_format:H:i',
-                'end_date' => 'required|date|after_or_equal:start_date',
-                'end_time' => 'required|date_format:H:i',
-                'color' => 'required|integer|between:1,6',
-                'content' => 'required|max:128',
-                'detail' => 'max:255',
-            ];
-        // 終了日が開始日以前の場合は開始時刻と終了時刻が前後していないか確認
-        }elseif($start_date >= $end_date){
-            return [
-                'start_date' => 'required|date',
-                'start_time' => 'required|date_format:H:i',
-                'end_date' => 'required|date|after_or_equal:start_date',
-                'end_time' => 'required|date_format:H:i|after_or_equal:start_time',
-                'color' => 'required|integer|between:1,6',
-                'content' => 'required|max:128',
-                'detail' => 'max:255',
+                'end_date'   => 'required|date|after_or_equal:start_date',
+                'end_time'   => 'required|date_format:H:i',
+                'color'      => 'required|integer|between:1,6',
+                'content'    => 'required|max:128',
+                'detail'     => 'max:255',
             ];
         }
+
+        // 終了日が開始日以前の場合は開始時刻と終了時刻が前後していないか確認
+        return [
+            'start_date' => 'required|date',
+            'start_time' => 'required|date_format:H:i',
+            'end_date'   => 'required|date|after_or_equal:start_date',
+            'end_time'   => 'required|date_format:H:i|after_or_equal:start_time',
+            'color'      => 'required|integer|between:1,6',
+            'content'    => 'required|max:128',
+            'detail'     => 'max:255',
+        ];
     }
 
+    /**
+     * パラメータ名を返す
+     * 
+     * @return array
+     */
     public function attributes()
     {
         return [
             'start_date' => '開始日',
             'start_time' => '開始時刻',
-            'end_date' => '終了日',
-            'end_time' => '終了時刻',
-            'content' => '内容',
-            'detail' => '詳細',
-            'color' => 'カラー',
+            'end_date'   => '終了日',
+            'end_time'   => '終了時刻',
+            'color'      => 'カラー',
+            'content'    => '内容',
+            'detail'     => '詳細',
         ];
     }
 }
